7c5d1b041ae6e3e837829f1d68d5dc15b9f2ad1b
Programador <PROGRA2016@outlook.es>
2016-11-09 19:45:07 -0600

    Plantilla

---
+++ MVC-BNG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_BNG.MvcApplication" Language="C#" %>
---
+++ MVC-BNG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_BNG.Startup))]
+namespace MVC_BNG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
